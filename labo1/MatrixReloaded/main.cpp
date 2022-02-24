128d5dc0235eed2f377a3051b65ead5f887abacf
Lazar <lazazr@gmail.com>
2022-02-24 17:21:25 +0100

    refactor: quick clean-up

--- labo1/MatrixReloaded/main.cpp
+++ labo1/MatrixReloaded/main.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
-#include "Matrix.hpp"
 #include <stdexcept>
+#include "Matrix.hpp"
 
 using namespace std;
 
-#define NUM_ARGS 5
+#define NUM_ARGS 6
 
-int main(int argc, char *argv[]) {
-   if (argc != NUM_ARGS + 1)
+int main(int argc, char* argv[]) {
+   if (argc != NUM_ARGS)
       throw invalid_argument("Erreur: Il manque des arguments.");
 
-   unsigned rows1;
-   unsigned cols1;
-   unsigned rows2;
-   unsigned cols2;
-   unsigned modulus;
+   unsigned rows1, cols1, rows2, cols2, modulus;
 
-   // TODO rendre clean
    try {
       rows1 = (unsigned) stoi(argv[1]);
       cols1 = (unsigned) stoi(argv[2]);
       rows2 = (unsigned) stoi(argv[3]);
       cols2 = (unsigned) stoi(argv[4]);
       modulus = (unsigned) stoi(argv[5]);
-   } catch (int e){
+   } catch (exception& e) {
       throw invalid_argument("Erreur: Un argument n'est pas une valeur correcte.");
    }
 
-
-	Matrix m1 = Matrix(rows1, cols1, modulus);
+   Matrix m1 = Matrix(rows1, cols1, modulus);
    Matrix m2 = Matrix(rows2, cols2, modulus);
-	cout << m1 << endl;
+   cout << m1 << endl;
    cout << m2 << endl;
-	return 0;
+   return 0;
 }
