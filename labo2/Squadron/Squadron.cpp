420f2694f961d3d87a59327d15a92baa8a8d5b4f
Lazar <lazazr@gmail.com>
2022-03-17 17:05:59 +0100

    feat: add squadron methods and properties

--- labo2/Squadron/Squadron.cpp
+++ labo2/Squadron/Squadron.cpp
@@ -1,5 +1,9 @@
 //
 // Created by Valentin Kaelin on 17.03.22.
 //
-
 #include "Squadron.hpp"
+
+using namespace std;
+
+Squadron::Squadron(const string& name) : name(name), size(0), leader(nullptr), head(nullptr),
+														next(nullptr) {}
--- labo2/Squadron/Squadron.hpp
+++ labo2/Squadron/Squadron.hpp
@@ -5,10 +5,35 @@
 #ifndef SQUADRON_SQUADRON_HPP
 #define SQUADRON_SQUADRON_HPP
 
+#include "Ship.hpp"
 
 class Squadron {
+	friend std::ostream& operator<<(std::ostream& lhs, const Squadron& rhs);
+	friend Squadron operator+(const Squadron& squad);
+	friend Squadron operator-(const Squadron& squad);
+public:
+	Squadron(const std::string& name);
+	Squadron(const Squadron& squad);
 
+	Squadron& operator+=(const Ship& ship);
+	Squadron& operator-=(const Ship& ship);
+	Squadron& operator[](unsigned index);
+
+	Ship get(unsigned index);
+	void setName(const std::string& name);
+	void setLeader(const Ship* ship); // S'il n'est pas dans la squad, on l'ajoute
+	void removeLeader();
+	unsigned computeConsumption(double distance, double speed);
+private:
+	struct Member {
+		Ship& ship;
+		Member* next;
+	};
+
+	const std::string name;
+	size_t size;
+	Ship* leader;
+	Member head;
 };
-
 
 #endif //SQUADRON_SQUADRON_HPP
