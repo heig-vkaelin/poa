9a0e257acd8dfe1a8691ee512c07aa7274010117
Kalane <vkaelin@hey.com>
2022-06-09 15:21:58 +0200

    feat: handle program args

--- labo4/Buffy/main.cpp
+++ labo4/Buffy/main.cpp
@@ -1,39 +1,47 @@
 #include <iostream>
-
-using namespace std;
-
 #include "Controller.hpp"
 #include "displayers/Displayer.hpp"
 #include "displayers/UnixDisplayer.hpp"
 #include "displayers/WindowsDisplayer.hpp"
 
-/**
- * Questions:
- * - random move de Buffy: 1 case
- */
+using namespace std;
 
 /**
  * @author Alexandre Jaquier
  * @author Valentin Kaelin
  */
 int main(int argc, char* argv[]) {
-	const unsigned SIZE = 50;
-	const unsigned NB_HUMANS = 20;
-	const unsigned NB_VAMPIRES = 10;
+	const int NB_ARGS = 5;
+
+	if (argc != NB_ARGS)
+		cout << "Usage: " << argv[0]
+			  << " <width> <height> <nbHumans> <nbVampires>" << endl;
+
+	unsigned width, height, nbHumans, nbVampires;
+	try {
+		for (int i = 1; i < argc; ++i) {
+			if (stoi(argv[i]) < 0) {
+				// Vérification que les différentes tailles ne sont pas négatives
+				throw exception();
+			}
+		}
+		width = (unsigned)stoi(argv[1]);
+		height = (unsigned)stoi(argv[2]);
+		nbHumans = (unsigned)stoi(argv[2]);
+		nbVampires = (unsigned)stoi(argv[3]);
+	} catch (exception& e) {
+		throw invalid_argument("Erreur: Un argument n'a pas une valeur positive.");
+	}
 
 #if  defined(__linux__) || defined(__APPLE__)
-	UnixDisplayer displayer(SIZE, SIZE);
+	UnixDisplayer displayer(width, height);
 #elif __WIN32
-	WindowsDisplayer displayer(SIZE, SIZE);
+	WindowsDisplayer displayer(width, height);
 #else
-	Displayer displayer(SIZE, SIZE);
+	Displayer displayer(width, height);
 #endif
 
-//	size_t size = strtoul(argv[1], nullptr, 0);
-//	size_t nbHumans = strtoul(argv[2], nullptr, 0);
-//	size_t nbVampires = strtoul(argv[3], nullptr, 0);
-
-	Controller controller(SIZE, SIZE, NB_HUMANS, NB_VAMPIRES, &displayer);
+	Controller controller(width, height, nbHumans, nbVampires, &displayer);
 	controller.run();
 
 	return EXIT_SUCCESS;
